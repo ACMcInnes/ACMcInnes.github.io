35a5d9e3f3c12d1b183575e6f3d42724c5734225
Andrew <acmcinnes@outlook.com>
2022-02-06 17:01:17 +1000

    Added Dark Mode Support
    Set up CSS variables to handle 'light' and 'dark' themes and setup media query to go with users browser preference on load.
    
    Added button to toggle theme if user prefers.

--- js/landing.js
+++ js/landing.js
@@ -98,3 +98,17 @@ function closeModal(){
         btn.classList.add("rotate-open");
     }
 }
+
+
+// Switch between colour schemes
+function themeToggle() {
+  // If the OS is set to dark mode...
+  if (prefersDarkScheme.matches) {
+    // ...then apply the .light-theme class to override those styles
+    document.body.classList.toggle("light-theme");
+    // Otherwise...
+  } else {
+    // ...apply the .dark-theme class to override the default light styles
+    document.body.classList.toggle("dark-theme");
+  }
+}
