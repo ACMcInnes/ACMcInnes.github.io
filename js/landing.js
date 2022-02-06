2817734a62c3e56f97331cb2982037b3a7431018
Andrew <acmcinnes@outlook.com>
2022-02-06 17:47:24 +1000

    Dark Theme Patch
    Updating additional styling for Dark Theme.

--- js/landing.js
+++ js/landing.js
@@ -102,13 +102,14 @@ function closeModal(){
 
 // Switch between colour schemes
 function themeToggle() {
-  // If the OS is set to dark mode...
-  if (prefersDarkScheme.matches) {
-    // ...then apply the .light-theme class to override those styles
-    document.body.classList.toggle("light-theme");
-    // Otherwise...
-  } else {
-    // ...apply the .dark-theme class to override the default light styles
-    document.body.classList.toggle("dark-theme");
-  }
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+    // If the OS is set to dark mode...
+    if (prefersDarkScheme.matches) {
+        // ...then apply the .light-theme class to override those styles
+        document.body.classList.toggle("light-theme");
+        // Otherwise...
+    } else {
+        // ...apply the .dark-theme class to override the default light styles
+        document.body.classList.toggle("dark-theme");
+    }
 }
